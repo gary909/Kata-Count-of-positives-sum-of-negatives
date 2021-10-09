0d8f528f75e20d93cc36c0638b54122898b7cf1a
gary909 <garywhite61@hotmail.com>
2021-10-09 13:37:27 +0200

    code working

--- kataCountPos.js
+++ kataCountPos.js
@@ -1,17 +1,28 @@
 function countPositivesSumNegatives(input) {
     var num = 0;
     var num2 = 0;
-    for (var i = 0; i < input.length; i++){
-        if (input[i] >= 1) {
-            num = num + 1;
-        } else if (input[i] < 0) {
-            num2 = num2 + input[i];
+    var newArr = [];
+    if (input == null || input.length < 1){
+        return [];
+      } else {
+        for (var i = 0; i < input.length; i++){
+            if (input[i] >= 1) {
+                num = num + 1;
+            } else if (input[i] < 0) {
+                num2 = num2 + input[i];
+            } else if (input[i] == 0) {
+                newArr == 'hello';
+            }
         }
+        newArr.push(num);
+        newArr.push(num2);
+        return newArr;
     }
-    console.log(num2);
 
 }
 
 
 
 console.log(countPositivesSumNegatives([0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14])) // 8, -50
+
+console.log(countPositivesSumNegatives([0, 0])) // []
